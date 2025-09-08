f79a2af8a8055ccd823ec6efa9c593e6fd0875a7
Caua Membribes <cauamembribesdelima@gmail.com>
2025-09-07 22:28:08 -0300

    add-sw

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
+if ('serviceWorker' in navigator){
+    navigator.serviceWorker.register('js/sw.js')
+    .then(registration => {
+        console.log('Service Worker registrado com sucesso!')
+    })
+    .catch(error => {
+        console.error('Registração de Service Worker malsucedido. :(')
+    })
+}
+
+const typeTranslate = {
+  normal: "Normal",
+  fire: "Fogo",
+  water: "Água",
+  grass: "Planta",
+  electric: "Elétrico",
+  ice: "Gelo",
+  fighting: "Lutador",
+  poison: "Veneno",
+  ground: "Terra",
+  flying: "Voador",
+  psychic: "Psíquico",
+  bug: "Inseto",
+  rock: "Pedra",
+  ghost: "Fantasma",
+  dark: "Sombrio",
+  dragon: "Dragão",
+  steel: "Aço",
+  fairy: "Fada"
+};
+
 let pokeName = document.querySelector(".pokemon_name")
 let pokeType = document.querySelector(".pokemon_type")
-
+let pokeHeight = document.querySelector(".pokemon_height")
+let pokeWeight = document.querySelector(".pokemon_weight")
 let pokeImage = document.getElementById("pokemonimage"); /* Quando comecei o projeto, eu tinha colocado essa variável dentro do escopo da função 
 e isso foi um problema, porque não me atentei, quando fui criar o checkbox de transformar em shiny, eu estava tentando manipular a variável que tinha sido
 instanciada dentro da função 'procurarPokemon', preciso me atentar mais em relação a isso.*/
@@ -15,6 +47,11 @@ function procurarPokemon () {
         ShinySrc = dados.sprites.other["official-artwork"].front_shiny;
         pokeImage.src = NormalSrc; // por padrão o pokeImage recebe o pokémon na forma normal dele.
         pokeName.innerHTML = `${dados.name} #${dados.id}`
+        pokeType.innerHTML = "Tipo: " + dados.types
+  .map(t => typeTranslate[t.type.name] || t.type.name)
+  .join(", ");
+        pokeHeight.innerHTML = `Height: ${dados.height / 10} m`
+        pokeWeight.innerHTML = `Peso: ${dados.weight / 10} kg`
         pokeCry.src = dados.cries.latest; 
         pokeCry.play(); // usei o .play() para poder tocar o audio
     })
--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,10 @@
 /* O Service Worker é um script que o navegador executa em segundo plano, ele funciona como 
-intermediário entre a aplicação e a redes,que permite funcionalidades como funcionar 
+intermediário entre a aplicação e a redes, que permite funcionalidades como funcionar 
 de forma offline, permite receber notificações mesmo que o app esteja fechado, 
 sincronização em segundo plano e etc*/
+
+self.addEventListener('install', event => {
+    console.log("Service Worker instalado.")
+})
+
 
